3c4ad7fe72f66baac7acb485f4f5c524917e35a2
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2015-12-10 23:31:27 +0100

    Extended the test data generator to create better timestamps

--- install-test-data.sh
+++ install-test-data.sh
@@ -3,9 +3,22 @@
 # todo: sercurity question/explicit user confirmation
 
 echo "Installing test data"
+echo "... we will wait 1s for each file to get a realistic file time"
 
 sh /usr/sleeptalk/scripts/clean-up.sh
 
-cp /usr/sleeptalk/test-data/* /usr/sleeptalk/records_raw
+DEMO_FILE_PATHS=/usr/sleeptalk/test-data/*
+
+set file_counter=0
+
+for demo_file_path in $DEMO_FILE_PATHS
+do
+	cp "$demo_file_path" /usr/sleeptalk/records_raw
+
+	echo "... copied $demo_file_path"
+
+	# todo: fake the creation time to save time while creating the test data
+	sleep 1s
+done
 
 echo "... done!"
