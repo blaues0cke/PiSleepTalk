ec737c90b7dd4d2749153d8515a8cb87afc69d6b
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-01-02 11:22:53 +0100

    Added some author headers (https://github.com/blaues0cke/PiSleepTalk/issues/15)

--- daemon.sh
+++ daemon.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# 
+# This file is part of PiSleepTalk.
+# Learn more at: https://github.com/blaues0cke/PiSleepTalk
+# 
+# Author:  Thomas Kekeisen <pisleeptalk@tk.ca.kekeisen.it>
+# License: This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+#          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+#
+
 # kFreeBSD do not accept scripts as interpreters, using #!/bin/sh and sourcing.
 if [ true != "$INIT_D_SCRIPT_SOURCED" ] ; then
     set "$0" "$@"; INIT_D_SCRIPT_SOURCED=true . /lib/init/init-d-script
--- install-test-data.sh
+++ install-test-data.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# 
+# This file is part of PiSleepTalk.
+# Learn more at: https://github.com/blaues0cke/PiSleepTalk
+# 
+# Author:  Thomas Kekeisen <pisleeptalk@tk.ca.kekeisen.it>
+# License: This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+#          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+#
+
 # todo: sercurity question/explicit user confirmation
 
 echo "Installing test data"
