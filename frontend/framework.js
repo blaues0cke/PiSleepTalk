c72757be332a2765689ce99eb5b6eddc8e99f39d
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-01-03 20:05:15 +0100

    Modified the filter to allow the final wav path

--- frontend/framework.js
+++ frontend/framework.js
@@ -10,7 +10,7 @@
 this.alphanumeric = function (input) {  
 	// Thanks to
 	// * http://www.w3resource.com/javascript/form/letters-numbers-field.php
-	var letterNumber = /^[0-9a-zA-Z]+$/;
+	var letterNumber = /^[0-9a-zA-Z_\-\.]+$/;
 
 	return input.match(letterNumber);
 }; 
