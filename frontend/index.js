1eae7e95cbb0e142cca9705bd3556599595a3fcb
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-01-27 20:53:01 +0100

    Moved the get-mp4 download route into a separted file (https://github.com/blaues0cke/PiSleepTalk/issues/48)

--- frontend/index.js
+++ frontend/index.js
@@ -76,20 +76,7 @@ app.use(function(req, res, next) {
 
 
 
-app.get('/download/:name' + defines.movieFileExtension, function(req, res) {
-	var filepath = checkFile(req, res, defines.movieFileExtension, 'records-final');
 
-	if (filepath) {
-		// Thanks to
-		// * http://stackoverflow.com/questions/7288814/download-a-file-from-nodejs-server-using-express
-		res.setHeader('Content-disposition', 'attachment; filename=' + req.params.name + defines.movieFileExtension);
-		res.setHeader('Content-type', 'video/' + defines.movieFileExtension.replace('.', ''));
-
-		// Thanks to
-		// * http://stackoverflow.com/questions/9321027/how-to-send-files-with-node-js
-		res.sendFile(filepath);
-	}
-});
 
 app.delete('/:name' + defines.movieFileExtension, function(req, res) {
 	var filepath = checkFile(req, res, defines.movieFileExtension, 'records-final');
---
+++ frontend/routes/get.mp4.download.js
@@ -0,0 +1,29 @@
+// 
+// This file is part of PiSleepTalk.
+//  Learn more at: https://github.com/blaues0cke/PiSleepTalk
+// 
+// Author:  Thomas Kekeisen <pisleeptalk@tk.ca.kekeisen.it>
+// License: This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+//
+
+var   defines   = require('../defines.js')
+	, framework = require('../framework.js')
+;
+
+module.exports = function(app) {
+	app.get('/download/:name' + defines.movieFileExtension, function(req, res) {
+		var filepath = framework.checkFile(req, res, defines.movieFileExtension, 'records-final');
+
+		if (filepath) {
+			// Thanks to
+			// * http://stackoverflow.com/questions/7288814/download-a-file-from-nodejs-server-using-express
+			res.setHeader('Content-disposition', 'attachment; filename=' + req.params.name + defines.movieFileExtension);
+			res.setHeader('Content-type', 'video/' + defines.movieFileExtension.replace('.', ''));
+
+			// Thanks to
+			// * http://stackoverflow.com/questions/9321027/how-to-send-files-with-node-js
+			res.sendFile(filepath);
+		}
+	});
+}
