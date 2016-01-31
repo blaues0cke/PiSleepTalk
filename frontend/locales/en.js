ce6a94e3f06e99648cb57a5efb8b15e20f505cb0
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-01-31 15:37:27 +0100

    The edit template is now localizable (https://github.com/blaues0cke/PiSleepTalk/issues/38)

--- frontend/locales/en.js
+++ frontend/locales/en.js
@@ -1,4 +1,27 @@
 {
+	"actions": {
+		"add_text": 			 "Add text",
+		"clear":                 "Clear",
+		"clear_all_texts":       "Clear all texts",
+		"decrease_volume":       "Decrease volume",
+		"delete": 				 "Delete",
+		"delete_text": 		     "Delete text",
+		"delete_recording": 	 "Delete recording",
+		"increase_volume":       "Increase volume",
+		"insert_current_frame":  "Insert current frame position",
+		"move_down": 			 "Move down",
+		"move_up": 				 "Move up",
+		"play":                  "Play",
+		"play_from_marker":      "Play from marker",
+		"save":                  "Save",
+		"save_text": 			 "Save text",
+		"set_marker":            "Set marker",
+		"sort":                  "Sort",
+		"sort_ascending":        "Sort ascending",
+		"step_backward":         "Step backward",
+		"step_forward":          "Step forward",
+		"stop":                  "Stop"
+	},
     "app": {
     	"name": 			     "PiSleepTalk"
     },
@@ -8,6 +31,35 @@
     	"status": 				 "Status",
     	"videos": 				 "Videos"
 
+    },
+    "modal": {
+    	"an_error_occurred":     "An error occurred",
+    	"cancel":                "Cancel",
+    	"clear_texts": {
+    		"clear":  			 "Clear",
+    		"description":       "Are you sure to delete all the text data?",
+    		"title":             "Clear text data"
+    	},
+    	"close":                 "Close",
+      	"decrease_volume": {
+    		"decrease_volume":   "Decrease volume",
+    		"description":       "Are you sure to decrease the volume of this audio file by 50%? This will move the file away from your processing scope until the volume is decreased. The audio file will automatically re-appear on the home page.",
+    		"title":             "Decrease volume"
+    	},
+       	"increase_volume": {
+    		"description":       "Are you sure to increase the volume of this audio file by 50%? This will move the file away from your processing scope until the volume is increased. The audio file will automatically re-appear on the home page.",
+    		"increase_volume":   "Increase volume",
+    		"title":             "Increase volume"
+    	},
+    	"remove_recording": {
+    		"delete_recording":  "Delete recording",
+    		"description":       "Are you sure to delete the whole recording? You can't undo this.",
+    		"title":             "Remove recording"
+    	},
+    	"set_marker": {
+    		"description":       "Please set a marker first before you try to jump to it.",
+    		"whoops":            "Whoops, sorry!"
+    	}
     },
     "shortcuts": {
     	"description":   	 	 "You can use some shortcuts to save time. The following keys are supported right now:",
@@ -23,5 +75,10 @@
     	},
     	"ok":            	 	 "OK",
     	"shortcuts":     	 	 "Shortcuts"
+    },
+    "table": {
+    	"actions": 			 	 "Actions",
+    	"frame": 				 "Frame",
+    	"text":       			 "Text"
     }
 }
