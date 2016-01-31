6666af5e4064d9c6548f0a915ca78868cc1641d0
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-01-31 16:30:13 +0100

    Added the german translation (https://github.com/blaues0cke/PiSleepTalk/issues/38)

---
+++ frontend/locales/de.js
@@ -0,0 +1,134 @@
+{
+	"actions": {
+		"add_text": 			 "Text hinzufügen",
+		"back":  				 "Zurück",
+		"clear":                 "Leeren",
+		"clear_all_texts":       "Alle Texte leeren",
+		"clear_logs": 			 "Logs löschen",
+		"decrease_volume":       "Lautstärke verringern",
+		"delete": 				 "Löschen",
+		"delete_file": 		 	 "Datei löschen",
+		"delete_recording": 	 "Aufnahme löschen",
+		"delete_text": 		     "Text löschen",
+		"delete_video": 	     "Video löschen",
+		"download_video": 		 "Video herunterladen",
+		"increase_volume":       "Lautstärke erhöhen",
+		"insert_current_frame":  "Aktuelle Frame-Position einfügen",
+		"move_down": 			 "Nach unten schieben",
+		"move_up": 				 "Nach oben schieben",
+		"play":                  "Abspielen",
+		"play_from_marker":      "Von Markierung spielen",
+		"play_video": 			 "Video im Browser abspielen",
+		"reload_page": 			 "Seite neuladen",
+		"save":                  "Speichern",
+		"save_text": 			 "Text speichern",
+		"set_marker":            "Markierung setzen",
+		"sort":                  "Sortieren",
+		"sort_ascending":        "Aufsteigend sortieren",
+		"step_backward":         "Schritt zurück",
+		"step_forward":          "Schritt vorwärts",
+		"stop":                  "Stop",
+		"view_logs": 			 "Logs betrachten"
+	},
+    "app": {
+    	"name": 			     "PiSleepTalk"
+    },
+    "logs": {
+    	"description": 			 "Nicht-aufbereitete Log-Dateien von node.js und anderen verwendeten Programmen wie ffmpeg oder sox.",
+    	"empty_text": 			 "Derzeit sind keine Log-Dateien verfügbar.",
+    	"title": 				 "Logs"
+    },
+    "menu": {
+    	"help":                  "Hilfe",
+    	"home": 				 "Start",
+    	"status": 				 "Status",
+    	"videos": 				 "Videos"
+
+    },
+    "modal": {
+    	"an_error_occurred":     "Es ist ein Fehler aufgetreten",
+    	"cancel":                "Abbrechen",
+    	"clear_logs": {
+    		"clear_logs":  		 "Log-Dateien löschen",
+    		"description":       "Bist du sicher, dass du alle Log-Dateien löschen möchtest? Diese Aktion kann nicht rückgängig gemacht werden.",
+    		"title":             "Log-Dateien löschen"
+    	},
+    	"clear_texts": {
+    		"clear":  			 "Leeren",
+    		"description":       "Bist du sicher, dass du alle Texte löschen möchtest?",
+    		"title":             "Texte löschen"
+    	},
+    	"close":                 "Schließen",
+      	"decrease_volume": {
+    		"decrease_volume":   "Lautstärke verringern",
+    		"description":       "Bist du sicher, dass du die Lautstärke dieser Datei um 50% verringern möchtest? Die Datei wird verschoben und unsichtbar, bis die Lautstärke verringert wurde. Danach taucht die Datei wieder automatisch auf der Startseite auf.",
+    		"title":             "Lautstärke verringern"
+    	},
+    	"delete_file": {
+    		"delete_file":   	 "Datei löschen",
+    		"description":       "Bist du sicher, dass du diese Datei löschen möchtest? Diese Aktion kann nicht rückgängig gemacht werden.",
+    		"title":             "Datei löschen"
+    	},
+       	"increase_volume": {
+    		"description":       "Bist du sicher, dass du die Lautstärke dieser Datei um 50% erhöhen möchtest? Die Datei wird verschoben und unsichtbar, bis die Lautstärke erhöht wurde. Danach taucht die Datei wieder automatisch auf der Startseite auf.",
+    		"increase_volume":   "Lautstärke erhöhen",
+    		"title":             "Lautstärke erhöhen"
+    	},
+    	"ok": 			  	     "OK",
+       	"preview_video": {
+    		"title":             "Video-Vorschau"
+    	},
+    	"remove_recording": {
+    		"delete_recording":  "Aufnahme löschen",
+    		"description":       "Bist du sicher, dass du diese Aufnahme löschen möchtest? Diese Aktion kann nicht rückgängig gemacht werden.",
+    		"title":             "Aufnahme löschen"
+    	},
+    	"remove_video": {
+    		"delete_video":  	 "Video löschen",
+    		"description":       "Bist du sicher, dass du dieses Video löschen möchtest? Diese Aktion kann nicht rückgängig gemacht werden.",
+    		"title":             "Video löschen"
+    	},
+    	"set_marker": {
+    		"description":       "Bitte setzte zu erst eine Markierung, bevor du zur selbigen springst.",
+    		"whoops":            "Hoppala, entschuldigung!"
+    	}
+    },
+    "no_recordings": {
+    	"description": 			 "Entschuldigung, es liegen keine unbearbeiteten Aufnahmen vor. Bitte schlafe eine Nacht darüber und kehre dann zur dieser Seite zurück.",
+    	"sub_description":       "Eventuell wirfst du einen Blick auf die Status-Seite um sicherzustellen, dass alles in Ordnung ist.",
+    	"title": 				 "Keine neuen Aufnahmen"
+    },
+    "shortcuts": {
+    	"description":   	 	 "You can use some shortcuts to save time. The following keys are supported right now:",
+    	"keys": {
+    		"1_9":  			 "1-9",
+    		"1_9_description": 	 "Springe zu Abschnitt N der Audio-Datei",
+    		"down_description":  "Springe 10 Sekunden vor",
+    		"left_description":  "Springe 5 Sekunden zurück",
+    		"right_description": "Springe 5 Sekunden vor",
+    		"space":     	 	 "Leertaste",
+    		"space_description": "Abspielen/Pause",
+    		"up_description":    "Springe 10 Sekunden zurück"
+    	},
+    	"ok":            	 	 "OK",
+    	"shortcuts":     	 	 "Tastenkürzel"
+    },
+    "status": {
+    	"description": 		     "Prüfe den Status deiner PiSleepTalk-Instanz. Seite generiert: %s",
+    	"disk_space": 			 "%s Speicherplatz von %s GB sind aktuell frei",
+    	"title":  				 "Status"
+    },
+    "table": {
+    	"actions": 			 	 "Aktionen",
+    	"frame": 				 "Frame",
+    	"length": 				 "Länge",
+    	"length_seconds_short":  "s",
+    	"path": 				 "Pfad",
+    	"size": 				 "Größe",
+    	"size_kb": 			     "KB",
+    	"text":       			 "Text"
+    },
+    "videos": {
+    	"empty_text": 			 "Derzeit liegen keine generierten Videos vor."
+    }
+}
