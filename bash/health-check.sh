83740b442c97baa517eee5181de9c766d87bf97d
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-02-07 16:58:08 +0100

    Its now possible to limit recording to specific hours (https://github.com/blaues0cke/PiSleepTalk/issues/63)

--- bash/health-check.sh
+++ bash/health-check.sh
@@ -30,6 +30,29 @@ if [ "$start_allowed" = true ]; then
 fi
 
 if [ "$start_allowed" = true ]; then
+	if [ "$recording_hours_check_enabled" = true ]; then
+		# Thanks to
+		# * http://www.cyberciti.biz/faq/unix-linux-bash-get-time/
+		current_hour=$(date +"%H")
+		time_check_result=$(echo "${recording_hours}" | grep -F -q -w "${current_hour}" && echo "1" || echo "0")
+
+		echo "... allowed hours: ${recording_hours}, current hour: ${current_hour}"
+
+		# Thanks to
+		# * http://stackoverflow.com/questions/8063228/how-do-i-check-if-a-variable-exists-in-a-list-in-bash
+		if [ "${time_check_result}" -gt "0" ]; then
+			echo "... time inside the allowed hours, start still allowed"
+		else
+			echo "... time outside the allowed hours, start forbidden"
+
+			start_allowed=false
+		fi
+	else
+		echo "... recording hours check disabled, skipping check"
+	fi
+fi
+
+if [ "$start_allowed" = true ]; then
 	if [ "$button_enabled" = true ]; then
 		echo "... gpio button support enabled, checking button"
 
