473d97d6a880f801cec8cac5c630dc3d20ffd08a
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-02-07 11:18:06 +0100

    Improved the health check to save some time if the start is forbidden (https://github.com/blaues0cke/PiSleepTalk/issues/49)

--- bash/health-check.sh
+++ bash/health-check.sh
@@ -15,56 +15,62 @@ start_allowed=true
 
 echo "Running health check"
 
-usedSpaceInPercent=$(df -k | head -2 | tail -1 | awk '{print $5}' | sed "s/\(\%\)//")
+if [ "$start_allowed" = true ]; then
+	usedSpaceInPercent=$(df -k | head -2 | tail -1 | awk '{print $5}' | sed "s/\(\%\)//")
 
-echo "... used space in percent: ${usedSpaceInPercent}%"
+	echo "... used space in percent: ${usedSpaceInPercent}%"
 
-if [ $usedSpaceInPercent -gt 90 ]; then
-	echo "... too much space is in use, stopping recording audio" 
+	if [ $usedSpaceInPercent -gt 90 ]; then
+		echo "... too much space is in use, stopping recording audio" 
 
-	start_allowed=false   
-else
-	echo "... enough free disk space available, validating recording service"
+		start_allowed=false   
+	else
+		echo "... enough free disk space available, validating recording service"
+	fi
 fi
 
-if [ "$button_enabled" = true ]; then
-	echo "... gpio button support enabled, checking button"
+if [ "$start_allowed" = true ]; then
+	if [ "$button_enabled" = true ]; then
+		echo "... gpio button support enabled, checking button"
 
-	button_state=$(gpio read 0)
+		button_state=$(gpio read 0)
 
-	echo "... button state: ${button_state}"
+		echo "... button state: ${button_state}"
 
-	if [ "${button_state}" -eq "1" ]; then
-		echo "... button on, start still allowed"
+		if [ "${button_state}" -eq "1" ]; then
+			echo "... button on, start still allowed"
+		else
+			echo "... button off, start forbidden"
+
+			start_allowed=false
+		fi
 	else
-		echo "... button off, start forbidden"
-
-		start_allowed=false
+		echo "... gpio button support disabled, skipping check"
 	fi
-else
-	echo "... gpio button support disabled, skipping check"
 fi
 
-if [ "$last_fm_enabled" = true ]; then
-	echo "... last.fm support enabled, checking last track date"
+if [ "$start_allowed" = true ]; then
+	if [ "$last_fm_enabled" = true ]; then
+		echo "... last.fm support enabled, checking last track date"
 
-	last_last_fm_time=$(curl -s 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='"${last_fm_username}"'&api_key='"${last_fm_api_key}"'&format=json' | python /usr/sleeptalk/bash/parse-lastfm.py)
-	current_time=$(date +%s)
-	difference=$(($current_time - $last_last_fm_time))
+		last_last_fm_time=$(curl -s 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='"${last_fm_username}"'&api_key='"${last_fm_api_key}"'&format=json' | python /usr/sleeptalk/bash/parse-lastfm.py)
+		current_time=$(date +%s)
+		difference=$(($current_time - $last_last_fm_time))
 
-	echo "... last fm time: ${last_last_fm_time}"
-	echo "... current time: ${current_time}"
-	echo "... difference: ${difference}"
+		echo "... last fm time: ${last_last_fm_time}"
+		echo "... current time: ${current_time}"
+		echo "... difference: ${difference}"
 
-	if [ "${difference}" -ge "${last_fm_timeout_in_seconds}" ]; then
-		echo "... last listened song is more that ${difference}s ago, start still allowed"
+		if [ "${difference}" -ge "${last_fm_timeout_in_seconds}" ]; then
+			echo "... last listened song is more that ${difference}s ago, start still allowed"
+		else
+			echo "... last listened song is less that ${difference}s ago, start forbidden"
+
+			start_allowed=false
+		fi
 	else
-		echo "... last listened song is less that ${difference}s ago, start forbidden"
-
-		start_allowed=false
+		echo "... last.fm support disabled, skipping check"
 	fi
-else
-	echo "... last.fm support disabled, skipping check"
 fi
 
 if [ "$start_allowed" = true ]; then
