9e8473e24e26a0f66d174689c460da4692fed8b8
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-02-18 22:03:13 +0100

    The node js server is now able to move files that should be cropped (https://github.com/blaues0cke/PiSleepTalk/issues/75)

---
+++ frontend/routes/post.crop.js
@@ -0,0 +1,28 @@
+// 
+// This file is part of PiSleepTalk.
+// Learn more at: https://github.com/blaues0cke/PiSleepTalk
+// 
+// Author:  Thomas Kekeisen <pisleeptalk@tk.ca.kekeisen.it>
+// License: This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+//
+
+var   config    = require('../core/config.js')
+	, framework = require('../core/framework.js')
+	, fs        = require('fs')
+;
+
+module.exports = function(app) {
+	app.post('/crop/:name.' + config.default_audio_format, function(req, res) {
+		var filepath = framework.checkFile(req, res, config.default_audio_format, config.audio_file_path_to_render);
+
+		if (filepath) {
+			// Thanks to
+			// * http://stackoverflow.com/questions/8579055/how-i-move-files-on-node-js
+			var newAudioPath = config.audio_file_path_crop + '/start-' + req.body.start + '-stop-' + req.body.end + '-' + req.params.name + '.' + config.default_audio_format;
+			fs.rename(filepath, newAudioPath);
+
+			res.status(200).send('OK');
+		}
+	});
+}
