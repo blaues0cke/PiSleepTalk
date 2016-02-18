56536bfda8148cd2d6da6d8b206f1f1706cace66
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-02-18 23:03:03 +0100

    Built the bash script that is trimming the audio (https://github.com/blaues0cke/PiSleepTalk/issues/75)

--- bash/crop-audio.sh
+++ bash/crop-audio.sh
@@ -26,11 +26,33 @@ if [ ! -d "${lock_file_name}" ]; then
 	for audio_file_path in $dir_list
 	do
 		if [ -f $audio_file_path ]; then
+			audio_file_name=$(basename $audio_file_path)
+
 		 	echo "... cropping file: ${audio_file_path}"
 
+			seconds_start=$(echo $audio_file_name | sed -r -n 's#^start-([0-9.]+)-stop-([0-9.]+).*?\.'"${default_audio_format}"'$#\1#p')
+		 	seconds_stop=$(echo $audio_file_name | sed -r -n 's#^start-([0-9.]+)-stop-([0-9.]+).*?\.'"${default_audio_format}"'$#\2#p')
+		 	# Thanks to
+		 	# * http://unix.stackexchange.com/questions/184360/how-can-subtract-2-floats-which-have-been-extracted-from-2-other-files-with-bash
+		 	duration=$(echo "${seconds_stop} ${seconds_start}" | awk '{print $1-$2}')
+		 	
+			echo "... seconds start: ${seconds_start}"
+			echo "... seconds stop: ${seconds_stop}"
+			echo "... duration: ${duration}"
 
+		 	audio_file_name=$(basename $audio_file_path)
+		 	final_audio_path="${audio_file_path_to_render}/CR-${audio_file_name}"
 
+		 	# Thanks to
+		 	# * http://stackoverflow.com/questions/12562023/trim-audio-files-with-sox-in-milliseconds
+		 	# * http://stackoverflow.com/questions/9667081/how-do-you-trim-the-audio-files-end-using-sox
+		 	sox $audio_file_path $final_audio_path trim "$seconds_start" "=${seconds_stop}" >>"${error_log_path}" 2>&1
 
+			if [ -f $final_audio_path ]; then
+		 		rm $audio_file_path
+			else
+				echo "... error while trimming audio, aborting"
+			fi
 
 		 	file_count=$((file_count + 1))
 
