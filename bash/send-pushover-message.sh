c438e0c8f4a41643c2195016ec3c383814a4fca7
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-07-24 12:11:47 +0200

    Push messages are now only sent when unprocessed files are available (https://github.com/blaues0cke/PiSleepTalk/issues/104)

--- bash/send-pushover-message.sh
+++ bash/send-pushover-message.sh
@@ -16,32 +16,33 @@ echo "Sending pushover message"
 if [ "$push_over_enabled" = true ]; then
 
 	unprocessedFilesPath="${audio_file_path_to_render}/*.${default_audio_format}"
-	unprocessedFiles=$(ls -l $unprocessedFilesPath | grep -v ^l | wc -l)
+	unprocessedFiles=$(ls -l $unprocessedFilesPath 2> /dev/null | grep -v ^l | wc -l)
 	usedSpaceInPercent=$(df -k | head -2 | tail -1 | awk '{print $5}' | sed "s/\(\%\)//")
 
-	echo "... path: ${unprocessedFilesPath}"
-	echo "... unprocessed files: ${unprocessedFiles}"
-	echo "... used space: ${usedSpaceInPercent}%"
+	if [ "${unprocessedFiles}" -gt "0" ]; then
+		echo "... path: ${unprocessedFilesPath}"
+		echo "... unprocessed files: ${unprocessedFiles}"
+		echo "... used space: ${usedSpaceInPercent}%"
 
-	echo "... initial push text: ${push_over_text}"
+		echo "... initial push text: ${push_over_text}"
 
-	push_over_text=$(echo "${push_over_text}" | sed 's/__unprocessed_files__/'"${unprocessedFiles}"'/g')
-	push_over_text=$(echo "${push_over_text}" | sed 's/__used_disk_space__/'"${usedSpaceInPercent}"'/g')
-	push_over_text=$(echo "${push_over_text}" | sed 's/%/%25/g')
-	push_over_text=$(echo "${push_over_text}" | sed 's/ /%20/g')
-	push_over_text=$(echo "${push_over_text}" | sed 's/,/%2C/g')
-	push_over_text=$(echo "${push_over_text}" | sed 's/\./%2E/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/__unprocessed_files__/'"${unprocessedFiles}"'/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/__used_disk_space__/'"${usedSpaceInPercent}"'/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/%/%25/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/ /%20/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/,/%2C/g')
+		push_over_text=$(echo "${push_over_text}" | sed 's/\./%2E/g')
 
-	echo "... final push text: ${push_over_text}"
+		echo "... final push text: ${push_over_text}"
 
-	final_push_payload="token=${push_over_token}&user=${push_over_user}&message=${push_over_text}"
+		final_push_payload="token=${push_over_token}&user=${push_over_user}&message=${push_over_text}"
 
-	echo "... final push payload: ${final_push_payload}"
+		echo "... final push payload: ${final_push_payload}"
 
-	curl -s --data "${final_push_payload}" http://api.pushover.net/1/messages.json > /dev/null
-
+		curl -s --data "${final_push_payload}" http://api.pushover.net/1/messages.json > /dev/null
+	else
+		echo "... no unprocessed files"
+	fi
 else
-
 	echo "... push over support is disabled"
-
 fi
