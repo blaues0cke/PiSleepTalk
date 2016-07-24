880ea01ed5719dfa050f9d2d17f687debe4daccc
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-07-24 15:11:39 +0200

    The server is now able to send png images (https://github.com/blaues0cke/PiSleepTalk/issues/113)

---
+++ frontend/routes/get.png.js
@@ -0,0 +1,24 @@
+// 
+// This file is part of PiSleepTalk.
+// Learn more at: https://github.com/blaues0cke/PiSleepTalk
+// 
+// Author:  Thomas Kekeisen <pisleeptalk@tk.ca.kekeisen.it>
+// License: This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+//
+
+var   config    = require('../core/config.js')
+	, framework = require('../core/framework.js')
+;
+
+module.exports = function(app) {
+	app.get('/:name.' + config.default_image_format, function(req, res) {
+		var filepath = framework.checkFile(req, res, config.default_image_format, [config.audio_file_path_to_render]);
+
+		if (filepath) {
+			// Thanks to
+			// * http://stackoverflow.com/questions/9321027/how-to-send-files-with-node-js
+			res.sendFile(filepath);
+		}
+	});
+}
