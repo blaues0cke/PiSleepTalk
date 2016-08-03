dd60043c460fe050544e593bbb652043dfe21729
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-08-04 00:10:07 +0200

    The "rename" command no longer creates annoying output in the console when the locale settings are broken (https://github.com/blaues0cke/PiSleepTalk/issues/114)

--- bash/tool/remove-whitespaces-from-path.sh
+++ bash/tool/remove-whitespaces-from-path.sh
@@ -7,9 +7,11 @@
 #          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 #
 
+. /usr/sleeptalk/config/config.cfg
+
 dir_path="$1"
 
 # Thanks to
 # * http://stackoverflow.com/questions/2709458/bash-script-to-replace-spaces-in-file-names
-find $dir_path -name "* *" -type d | rename 's/ /_/g'
-find $dir_path -name "* *" -type f | rename 's/ /_/g'
+find $dir_path -name "* *" -type d | rename 's/ /_/g' >>"${error_log_path}" 2>&1
+find $dir_path -name "* *" -type f | rename 's/ /_/g' >>"${error_log_path}" 2>&1
