fb8997d55403af2592bf8bb0a9eeb1aff3f0f8b5
Valentin Radulescu <radulescu.valentin@gmail.com>
2016-10-30 02:15:24 +0300

    Add initial Italian translation #169

---
+++ frontend/locales/it.js
@@ -0,0 +1,265 @@
+{
+  "actions": {
+    "add_text": "Aggiungere testo",
+    "back": "Indietro",
+    "ban_noise": "Selezione di Mark come rumore",
+    "clear": "Chiaro",
+    "clear_all_texts": "Deselezionare tutti i testi",
+    "clear_logs": "Cancellare i registri",
+    "clear_pending": "Eliminare file in attesa",
+    "concat_all_videos": "Concatena tutti i video",
+    "concat_checked_videos": "Video di Concat selezionato",
+    "crop_to_marker": "Raccolto al marcatore",
+    "decrease_volume": "Diminuire il volume",
+    "delete": "Elimina",
+    "delete_file": "Elimina file",
+    "delete_noise_filter": "Elimina filtro rumore",
+    "delete_recording": "Cancellare registrazione",
+    "delete_text": "Eliminare il testo",
+    "delete_video": "Eliminare video",
+    "disable_preview": "Disabilitare la modalità anteprima",
+    "download_video": "Scaricare video",
+    "enable_preview": "Abilitare la modalità anteprima",
+    "force_import": "Importazione di forza ora",
+    "import_audio": "Importare le registrazioni",
+    "increase_volume": "Aumentare il volume",
+    "insert_current_frame": "Inserire la posizione corrente del telaio",
+    "jump_to_marker": "Passare al marcatore",
+    "move_down": "Spostare verso il basso",
+    "move_up": "Spostarsi verso l'alto",
+    "pause": "Pausa",
+    "play": "Gioca",
+    "play_from_here": "Giocare da qui",
+    "play_from_marker": "Gioca da marcatore",
+    "play_video": "Riprodurre il video nel browser",
+    "reload_page": "Ricarica la pagina",
+    "save": "Salva",
+    "save_text": "Salvare il testo",
+    "set_marker": "Imposta marcatore",
+    "set_playback_rate": "Velocità di riproduzione set",
+    "sort": "Ordinamento",
+    "sort_ascending": "Ordina in senso crescente",
+    "step_backward": "Passo indietro",
+    "step_forward": "Un passo avanti",
+    "stop": "Fermata",
+    "upload_file": "Caricare file",
+    "view_logs": "Visualizzare i registri",
+    "view_noise_filters": "Filtri di visualizzazione rumore"
+  },
+  "app": {
+    "name": "PiSleepTalk"
+  },
+  "import": {
+    "description": "Utilizzando la funzione di importazione consente di importare qualsiasi file audio che si desidera. È possibile utilizzare qualsiasi formato audio che si desidera. File non supportati vengono ignorati ed eliminati automaticamente. I file zip sono estratte automaticamente, troppo. Se i file nel file zip hanno lo stesso nome come file esistenti nella cartella di importazione, il file esistente verrà sovrascritto senza chiedere.",
+    "empty": "La cartella di importazione è vuota al momento.",
+    "error_1": "Nessun file è stato caricato.",
+    "error_2": "Errore durante il salvataggio del file, si prega di riprovare più tardi.",
+    "force_text": "L'importazione si avvia automaticamente dopo l'ultimo file è stato caricato nella cartella di importazione in %s secondi.",
+    "instant_approve": "Utilizzare i file importati come registrazioni complete senza postprocessing",
+    "title": "Importazione"
+  },
+  "logs": {
+    "description": "RAW file di node. js e alcuni utilizzati programmi come ffmpeg o sox.",
+    "empty_text": "Nessun dato di registro è disponibile al momento.",
+    "title": "Registri"
+  },
+  "menu": {
+    "clear_pending": "Eliminare file in attesa",
+    "help": "Guida",
+    "home": "Casa",
+    "import": "Importazione",
+    "overview": "Panoramica",
+    "status": "Stato",
+    "videos": "Video"
+  },
+  "modal": {
+    "an_error_occurred": "Si è verificato un errore",
+    "ban_noise": {
+      "ban_noise": "Selezione di Mark come rumore",
+      "description": "Sei sicuro che si desidera contrassegnare la selezione come rumore?",
+      "title": "Contrassegnare come rumore"
+    },
+    "cancel": "Annulla",
+    "clear_logs": {
+      "clear_logs": "Cancellare i registri",
+      "description": "Sei sicuro di cancellare tutti i file di registro? Questo non può essere annullato.",
+      "title": "Cancellare i registri"
+    },
+    "clear_pending": {
+      "clear": "Cancella file non trasformati",
+      "description": "Sei sicuro di cancellare tutti i file non elaborati? Questo non può essere annullato.",
+      "title": "Cancella file non trasformati"
+    },
+    "clear_texts": {
+      "clear": "Chiaro",
+      "description": "Sei sicuro di voler eliminare tutti i dati di testo?",
+      "title": "Testo in chiaro"
+    },
+    "close": "Chiudere",
+    "concat_videos": {
+      "concat_videos": "Video di Concat",
+      "description": "Sei sicuro di concat i video? Il selezionato scomparirà fino a quando non viene eseguito il rendering del filmato finale.",
+      "title": "Video di Concat per film pieno"
+    },
+    "crop_to_marker": {
+      "crop_to_marker": "Delle colture",
+      "description": "Sei sicuro ritagliare il file audio per l'area contrassegnata? Questo non può essere annullato.",
+      "title": "Raccolto al marcatore"
+    },
+    "decrease_volume": {
+      "decrease_volume": "Diminuire il volume",
+      "description": "Sei sicuro diminuire il volume del file audio del 50%? Questo sposterà il file dal vostro ambito di elaborazione fino a quando il volume viene diminuito. Il file audio automaticamente apparirà nuovamente nella home page.",
+      "title": "Diminuire il volume"
+    },
+    "delete_file": {
+      "delete_file": "Elimina file",
+      "description": "Sei sicuro di voler eliminare questo file? Non è possibile annullare questo.",
+      "title": "Elimina file"
+    },
+    "force_import": {
+      "description": "Sei sicuro, per forzare l'importazione in questo momento?",
+      "force_import": "Importazione di forza",
+      "title": "Importazione di forza"
+    },
+    "forced_import": {
+      "description": "L'importatore è stato avviato correttamente, si prega di attendere fino a quando la cartella di importazione è vuota.",
+      "title": "Importazione forzata"
+    },
+    "increase_volume": {
+      "description": "Sei sicuro aumentare il volume del file audio del 50%? Questo sposterà il file dal vostro ambito di elaborazione fino a quando il volume è aumentato. Il file audio automaticamente apparirà nuovamente nella home page.",
+      "increase_volume": "Aumentare il volume",
+      "title": "Aumentare il volume"
+    },
+    "noise_banned": {
+      "description": "La parte selezionata è ora recongized come rumore e rimosso in ulteriori registrazioni.",
+      "title": "Rumore salvato"
+    },
+    "ok": "Ok",
+    "preview_video": {
+      "title": "Anteprima video"
+    },
+    "remove_noise_filter": {
+      "description": "Sei sicuro di rimuovere questo filtro di rumore? Questo non può essere annullato. File, che sono stati già modificati utilizzando questo filtro rumore rimarrà invariati.",
+      "remove_filter": "Rimuovere il filtro di rumore",
+      "title": "Rimuovere il filtro di rumore"
+    },
+    "remove_recording": {
+      "delete_recording": "Cancellare registrazione",
+      "description": "Sei sicuro di voler cancellare l'intera registrazione? Non è possibile annullare questo.",
+      "title": "Rimuovere la registrazione"
+    },
+    "remove_video": {
+      "delete_video": "Eliminare video",
+      "description": "Sei sicuro di voler eliminare l'intero video? Non è possibile annullare questo.",
+      "title": "Rimuovere video"
+    },
+    "set_frame": {
+      "description": "Prima di provare a giocare da esso, si prega di impostare un frame prima.",
+      "whoops": "Ops, Siamo spiacenti!"
+    },
+    "set_marker": {
+      "description": "Prima di provare a saltare ad esso, si prega di impostare un marcatore prima.",
+      "description_2": "Prima di provare a saltare ad esso, si prega di impostare un marcatore prima.",
+      "description_3": "Si prega di impostare un marcatore in primo luogo prima di provare a ritagliare la registrazione ad esso.",
+      "description_4": "Si prega di impostare un marcatore prima prima è contrassegnare come rumore.",
+      "whoops": "Ops, Siamo spiacenti!"
+    },
+    "set_playback_rate": {
+      "description": "Inserire un nuovo tasso di riproduzione qui. Valore predefinito è il numero \"1.0\", un numero più piccolo è più lento, un numero più grande è più veloce.",
+      "set_playback_rate": "Velocità di riproduzione set",
+      "tip": "Standard: 1.0, più lento: < 1.0, più veloce: > 1.0",
+      "title": "Velocità di riproduzione"
+    },
+    "upload_file": {
+      "description": "Caricare un file audio o zip, che contiene i file audio, nella cartella di importazione.",
+      "title": "Caricare file",
+      "upload_file": "Caricare file"
+    }
+  },
+  "no_recordings": {
+    "description": "Siamo spiacenti, non abbiamo nessun registrazioni non trasformati per te in questo momento. Si prega di dormire su di esso il tornare a questa pagina più tardi.",
+    "sub_description": "Si può controllare la pagina di stato per assicurarsi che tutto funzioni correttamente.",
+    "title": "Nessun nuove registrazioni"
+  },
+  "noise_filter": {
+    "description": "Qui vedete tutti i filtri di rumore che è stato creato. Ogni filtro singolo rumore viene aggiunto per ogni nuova registrazione.",
+    "empty_text": "Nessun rumore filtri sono erano ancora. Per aggiungere un filtro, basta impostare un marcatore sulla pagina iniziale e premere il pulsante \"Crea filtro rumore\".",
+    "title": "Filtri anti rumore"
+  },
+  "overview": {
+    "description": "Ecco un elenco di tutte le registrazioni non trasformati. È possibile utilizzare questo elenco per eliminare i file che non si desidera elaborare in futuro. Questa pagina è limitata a %s voci causa alcuni problemi di prestazioni. Tenere premuto il tasto MAIUSC mentre si fa clic sul pulsante Elimina per saltare la domanda di sicurezza.",
+    "empty_text": "Nessuna registrazione non trasformata sono disponibile al momento.",
+    "title": "Panoramica"
+  },
+  "shortcuts": {
+    "description": "È possibile utilizzare alcune scorciatoie per risparmiare tempo. I seguenti tasti sono supportati in questo momento:",
+    "keys": {
+      "1_9": "1-9",
+      "1_9_description": "Cercano di parte N del file audio",
+      "b": "B",
+      "b_description": "Selezione di bandiera come rumore",
+      "c": "C",
+      "c_description": "Raccolto al marcatore (buttare via il resto)",
+      "d": "D",
+      "d_description": "Eliminare la registrazione corrente (tenere premuto MAIUSC per disattivare la domanda di sicurezza)",
+      "dc": "Fare doppio clic",
+      "dc_description": "Saltare per posizionare e creare testo",
+      "down": "Freccia giù",
+      "down_description": "Skip avanti di 10 secondi",
+      "esc": "ESC",
+      "esc_description": "Fermata",
+      "f": "F",
+      "f_description": "Attiva/disattiva modalità di anteprima",
+      "j": "J",
+      "j_description": "Passare al marcatore (e smettere di giocare)",
+      "left": "Freccia sinistra",
+      "left_description": "Skip indietro 5 secondi",
+      "m": "M",
+      "m_description": "Imposta marcatore",
+      "p": "P",
+      "p_description": "Gioca da marcatore",
+      "right": "Freccia a destra",
+      "right_description": "Skip forward 5 secondi",
+      "space": "Spazio",
+      "space_description": "Play/pausa",
+      "up": "Freccia in su",
+      "up_description": "Saltare indietro di 10 secondi"
+    },
+    "ok": "Ok",
+    "shortcuts": "Tasti di scelta rapida"
+  },
+  "state": {
+    "not_recording": "Non registrazione",
+    "recording": "Registrazione"
+  },
+  "status": {
+    "description": "Esaminare lo stato dell'istanza PiSleepTalk. Pagina generata: %s",
+    "disk_space": "Spazio su disco libero %s di %s GB",
+    "empty_text": "Non ci sono nessun file audio o video disponibili in questo momento.",
+    "pending_files": "File in sospeso",
+    "title": "Stato"
+  },
+  "table": {
+    "actions": "Azioni",
+    "concat": "Concat",
+    "frame": "Telaio",
+    "length": "Lunghezza",
+    "length_seconds_short": "s",
+    "listen": "Ascolta",
+    "path": "Percorso",
+    "size": "Dimensioni",
+    "size_kb": "KB",
+    "text": "Testo",
+    "type": "Tipo"
+  },
+  "videos": {
+    "description": "Qui si vede che tutto il rendering video il sistema registrato e generato per voi. La lunghezza di tutte le scene disponibili in pochi secondi è: %s.",
+    "empty_text": "Nessun video resi disponibili al momento.",
+    "movie_title": "Titolo video",
+    "movie_title_tip": "Il titolo del film apparirà per %s secondi all'inizio.",
+    "sub_description": "Puoi concat video selezionando la casella di controllo e premere il pulsante \"Concat video\".",
+    "title": "Video",
+    "type_full": "Video",
+    "type_part": "Scena"
+  }
+}
